8c5723b102469fdbde20ca7d659714af7fa64bb9
kaister3 <kaister3@protonmail.com>
2023-03-06 16:58:35 -0800

    fix ForecastApplication class path issue

--- forecast/src/main/java/com/lemon/forecast/ForecastApplication.java
+++ forecast/src/main/java/com/lemon/forecast/ForecastApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ImportResource(value = {"classpath:META-INF/spring/bean.xml"})
 public class ForecastApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ForecastApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ForecastApplication.class, args);
+  }
 }
